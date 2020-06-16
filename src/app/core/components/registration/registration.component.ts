4d7e1848c6765b4e0056ff793523de090811686a
rafalekqaz <rafaldinho11@gmail.com>
2020-06-16 20:35:17 +0200

    structure for registration form

--- src/app/core/components/registration/registration.component.ts
+++ src/app/core/components/registration/registration.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Role} from '../../_models/role.enum';
 
 @Component({
   selector: 'app-registration',
@@ -7,20 +8,39 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
-
   userRegistrationFormGroup: FormGroup;
   jobSeekerFormGroup: FormGroup;
   employerFormGroup: FormGroup;
-
+  usersRole: any;
   constructor(private formBuilder: FormBuilder) {}
 
 
   ngOnInit() {
+    this.initRole();
     this.userRegistrationFormGroup = this.formBuilder.group({
-      firstCtrl: ['', Validators.required]
+      login: ['', [Validators.required, Validators.minLength(3)]],
+      role: ['', Validators.required]
     });
+    this.userRegistrationFormGroup.get('role').setValue(Role.jobSeeker);
     this.jobSeekerFormGroup = this.formBuilder.group({
-      secondCtrl: ['', Validators.required]
+      secondCtrl: [Role.jobSeeker, Validators.required]
     });
+  }
+  get role() {
+    return this.jobSeekerFormGroup.get('role');
+  }
+
+  initRole(){
+    const {jobSeeker, employer} = Role;
+    this.usersRole = {
+      jobSeeker : {
+        value : jobSeeker,
+        displayValue : 'Szukam pracy'
+      },
+      employer : {
+        value : employer,
+        displayValue : 'Jestem pracodawcą'
+      }
+    };
   }
 }
